045f8793a63843b9cdfc2147a6aa196f45d56660
Mr.Robot <31414964+1nfty@users.noreply.github.com>
2021-02-24 15:32:48 +0800

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,4 @@
-<script type="text/javascript">
+
     $.extend({
         message: function (a) {
             var b = {};
@@ -23,4 +23,4 @@
             }, b.time))
         }
     })
-</script>
+
