b16eac61587a61d61e2d38cfc9f2c418b07502eb
Mr.Robot <31414964+1nfty@users.noreply.github.com>
2021-02-24 13:30:30 +0800

    Create test.js

---
+++ test.js
@@ -0,0 +1,26 @@
+<script type="text/javascript">
+    $.extend({
+        message: function (a) {
+            var b = {};
+            "string" == typeof a && (b.message = a), "object" == typeof a && (b = $.extend({}, b, a));
+            var c, d, e, f = b.showClose ? '<div class="c-message--close">×</div>' : "",
+                g = "" !== b.title ? '<h2 class="c-message__title">' + b.title + "</h2>" : "",
+                h = '<div class="c-message animated animated-lento slideInRight"><i class=" c-message--icon c-message--' + b.type + '"></i><div class="el-notification__group">' + g + '<div class="el-notification__content">' + b.message + "</div>" + f + "</div></div>",
+                i = $("body"),
+                j = $(h);
+            d = function () {
+                j.addClass("slideOutRight"), j.one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function () {
+                    e()
+                })
+            }, e = function () {
+                j.remove(), b.onClose(b), clearTimeout(c)
+            }, $(".c-message").remove(), i.append(j), j.one("webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend", function () {
+                j.removeClass("messageFadeInDown")
+            }), i.on("click", ".c-message--close", function (a) {
+                d()
+            }), b.autoClose && (c = setTimeout(function () {
+                d()
+            }, b.time))
+        }
+    })
+</script>
